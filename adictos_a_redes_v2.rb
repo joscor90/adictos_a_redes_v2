bbaddca1468b86a85f5b5121bbac1db802e2fa39
Jose Cordero Fuentealba <jocorderf@gmail.com>
2020-08-31 18:50:15 -0400

    scan_addicts2 method was added

---
+++ adictos_a_redes_v2.rb
@@ -0,0 +1,13 @@
+#scan_addicts2 method
+def scan_addicts2(arr)
+    new_arr = []
+    arr.each do |e|
+        if e < 90
+            new_arr.push("bien")
+        elsif e < 180 && e >= 90
+            new_arr.push("mejorable")
+        else
+            new_arr.push("mal")
+        end
+    end
+end
